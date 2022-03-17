9a127b712997b9e13371c9c97e14adb469952017
rasel-hossen <straselhossen24@gmail.com>
2022-03-17 23:48:22 +0600

    third commit

--- lib/Screen/home.dart
+++ lib/Screen/home.dart
@@ -20,7 +20,6 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.green,
         title: Text("Home"),
       ),
       body: Container(
